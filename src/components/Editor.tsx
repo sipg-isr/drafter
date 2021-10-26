f811ad096d662858e19e868564d93f8cba74ab54
Andrew Lee <andrewtl@stanford.edu>
2021-10-26 20:43:22 +0100

    feat(editor): set up very basic element view

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   forceSimulation,
   forceLink,
@@ -13,43 +13,100 @@ import {
   Form
 } from 'react-bootstrap';
 import { List } from 'immutable';
-import { Model } from '../types';
+import { Model, RemoteMethod } from '../types';
+import { FaPlus } from 'react-icons/fa';
 
 interface SidebarProps {
   models: List<Model>;
+  addNode: (model: Model) => void;
 };
-function Sidebar({ models }: SidebarProps) {
+function Sidebar({ models, addNode }: SidebarProps) {
   return (
     <>
       <h6>Available models</h6>
       <ListGroup>
         {models.map(model =>
-        <ListGroup.Item>{model.name}</ListGroup.Item>
+        <ListGroup.Item key={model.name}>
+          <Button onClick={() => addNode(model)}>
+            <FaPlus />&nbsp;{model.name}
+          </Button>
+        </ListGroup.Item>
         )}
       </ListGroup>
     </>
   );
 }
 
-interface GraphProps {};
-function Graph() {
+interface GraphProps {
+  nodes: List<Model>;
+  setNodes: (nodes: List<Model>) => void;
+};
+function Graph({ nodes, setNodes }: GraphProps) {
+  const [simulation] = useState(forceSimulation<Model | RemoteMethod>().stop());
+  // TODO make these configurable?
+  const width = 400;
+  const height = 800;
+
+  useEffect(() => {
+    const methods = nodes.flatMap(node => node.methods);
+    console.log(nodes, methods);
+    simulation.nodes(nodes.concat(methods).toArray());
+    simulation.force('charge', forceManyBody().strength(-2000));
+    // simulation.alpha(1);
+    simulation.alphaTarget(0.0).restart();
+  }, [nodes]);
+
+  simulation.on('tick', () => {
+    setNodes(List(nodes));
+  });
+
   return (
-    <h6>Graph</h6>
+    <svg
+      width={width}
+      height={height}
+      style={{
+        border: '1px solid black'
+      }}
+      viewBox={`0 0 ${width} ${height}`}
+    >
+      {nodes.map(cn => <ModelSVG model={cn} />)}
+    </svg>
   );
 }
+
+interface ModelSVGProps {
+  model: Model;
+};
+function ModelSVG({ model: { x, y} } : ModelSVGProps) {
+  return (
+    <circle
+      r={20}
+      cx={x!}
+      cy={y!}
+      stroke="#000"
+      strokeWidth="4px"
+    />
+  );
+};
 
 interface EditorProps {
   models: List<Model>;
 };
 export default function Editor({ models }: EditorProps) {
-  const [text, setText] = useState('');
+  const [nodes, setNodes] = useState<List<Model>>(List([{
+    name: 'hello',
+    image: 'none',
+    methods: []
+  }]));
   return (
     <>
       <Row>
         <Col xs="2">
-          <Sidebar models={models} />
+          <Sidebar models={models} addNode={(node: Model) => {
+            setNodes(nodes.push(node))
+          }} />
         </Col>
-        <Col><Graph /></Col>
+        <Col><Graph nodes={nodes} setNodes={setNodes} /></Col>
       </Row>
     </>
   );
--- src/types.ts
+++ src/types.ts
@@ -1,14 +1,20 @@
+import { SimulationNodeDatum, SimulationLinkDatum } from 'd3-force';
 import { IType } from 'protobufjs';
 
 export type MessageType = IType & { name: string };
-export interface RemoteMethod {
+/**
+ * An RPC method that can be called on a compute node
+ */
+export interface RemoteMethod extends SimulationNodeDatum {
   name: string;
   requestType: MessageType;
   responseType: MessageType
 };
 
-// this defines a model, which is a template from which nodes in the editor can be made
-export interface Model {
+/**
+ * this defines a model, which is a template from which nodes in the editor can be made
+ */
+export interface Model extends SimulationNodeDatum {
   name: string;
   // An identifying image name, like sipgisr/image-source:latest
   image: string;
@@ -16,3 +22,4 @@ export interface Model {
   methods: RemoteMethod[];
 }
 
+// TODO also create nodes for the node remoteMethods
