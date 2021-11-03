4407bcb88eda30429414f9d2f79221aecf7daff1
Andrew Lee <andrewtl@stanford.edu>
2021-11-03 13:32:49 +0000

    misc(ui): decompose out EditModel from Models.tsx

---
+++ src/components/EditModel.tsx
@@ -0,0 +1,82 @@
+import React, { useRef, useState } from 'react';
+import {
+  Button,
+  Form,
+} from 'react-bootstrap';
+import { List, Set } from 'immutable';
+import { Model } from '../types';
+import { protobufToRemoteMethods } from '../utils';
+import { FaPlus } from 'react-icons/fa';
+
+interface EditModelProps {
+  addModel: (model: Model) => void;
+}
+export default function EditModel({ addModel }: EditModelProps) {
+  const [name, setName] = useState('');
+  const [image, setImage] = useState('');
+  const filesRef = useRef<HTMLInputElement | null>(null);
+  // A function that returns the plain-text value of the currently-uploaded file
+  const fileContent: () => Promise<string | null> = async () => {
+    // Get the set of files associated with the current file input
+    // Note that we coerce to undefined in case of a falsy value here because the `Set`
+    // constructor does not accept null.
+    const files = List(filesRef?.current?.files || undefined);
+    if (files) {
+      // If the thing actually exists...
+      // We should make sure it has exactly one file
+      if (files.size === 1) {
+        // We can assert-nonnull here because we know the files list has a first element
+        const file = files.first()!;
+        return file.text();
+      } else {
+        console.error(`Attempted to upload more than one protobuf file for a model. Files were [${files.map(file => file.name).join(', ')}]}`);
+        return null;
+      }
+    } else {
+      return null;
+    }
+  };
+  return (
+    <>
+      <tr>
+        <td>
+          <Form.Control
+            placeholder='My model name'
+            value={name}
+            onChange={event => setName(event.target.value)}
+          />
+        </td>
+        <td>
+          <Form.Control
+            placeholder='<dockerid>/<reponame>'
+            value={image}
+            onChange={event => setImage(event.target.value)}
+          />
+        </td>
+        <td>
+          <Form.Control
+            type='file'
+            ref={filesRef}
+            accept='.proto'
+          />
+        </td>
+        <td>
+          <Button
+            variant='success'
+            onClick={async () => {
+              addModel({
+                name,
+                image,
+                // TODO show an error message and abort if this is null
+                methods: protobufToRemoteMethods(await fileContent() || '') || Set()
+              });
+              // Clear all the fields
+              setName('');
+              setImage('');
+              // TODO delete the file ref
+            }}><FaPlus /></Button>
+        </td>
+      </tr>
+    </>
+  );
+}
--- src/components/Models.tsx
+++ src/components/Models.tsx
@@ -1,14 +1,14 @@
-import React, { useRef, useState } from 'react';
+import React from 'react';
 import {
   Button,
-  Form,
   Table
 } from 'react-bootstrap';
-import { List, Set } from 'immutable';
-import { FaPlus, FaTrash } from 'react-icons/fa';
+import { List } from 'immutable';
+import {  FaTrash } from 'react-icons/fa';
 // Import types
 import { Model } from '../types';
-import { protobufToRemoteMethods, remoteMethodToString } from '../utils';
+import { remoteMethodToString } from '../utils';
+import EditModel from './EditModel';
 
 interface ModelViewProps {
   model: Model;
@@ -27,78 +27,6 @@ function ModelView({ model: { name, image, methods }, removeModel }: ModelViewPr
   );
 }
 
-interface ModelAddingFormProps {
-  addModel: (model: Model) => void;
-}
-function ModelAddingForm({ addModel }: ModelAddingFormProps) {
-  const [name, setName] = useState('');
-  const [image, setImage] = useState('');
-  const filesRef = useRef<HTMLInputElement | null>(null);
-  // A function that returns the plain-text value of the currently-uploaded file
-  const fileContent: () => Promise<string | null> = async () => {
-    // Get the set of files associated with the current file input
-    // Note that we coerce to undefined in case of a falsy value here because the `Set`
-    // constructor does not accept null.
-    const files = List(filesRef?.current?.files || undefined);
-    if (files) {
-      // If the thing actually exists...
-      // We should make sure it has exactly one file
-      if (files.size === 1) {
-        // We can assert-nonnull here because we know the files list has a first element
-        const file = files.first()!;
-        return file.text();
-      } else {
-        console.error(`Attempted to upload more than one protobuf file for a model. Files were [${files.map(file => file.name).join(', ')}]}`);
-        return null;
-      }
-    } else {
-      return null;
-    }
-  };
-  return (
-    <>
-      <tr>
-        <td>
-          <Form.Control
-            placeholder='My model name'
-            value={name}
-            onChange={event => setName(event.target.value)}
-          />
-        </td>
-        <td>
-          <Form.Control
-            placeholder='<dockerid>/<reponame>'
-            value={image}
-            onChange={event => setImage(event.target.value)}
-          />
-        </td>
-        <td>
-          <Form.Control
-            type='file'
-            ref={filesRef}
-            accept='.proto'
-          />
-        </td>
-        <td>
-          <Button
-            variant='success'
-            onClick={async () => {
-              addModel({
-                name,
-                image,
-                // TODO show an error message and abort if this is null
-                methods: protobufToRemoteMethods(await fileContent() || '') || Set()
-              });
-              // Clear all the fields
-              setName('');
-              setImage('');
-              // TODO delete the file ref
-            }}><FaPlus /></Button>
-        </td>
-      </tr>
-    </>
-  );
-}
 
 interface ModelsProps {
   models: List<Model>;
@@ -120,7 +48,7 @@ export default function Models({ models, setModels }: ModelsProps) {
           {models.map(model => <ModelView key={model.name + Math.random()} model={model} removeModel={() => {
             setModels(models.remove(models.indexOf(model)));
           }} />)}
-          <ModelAddingForm addModel={(model: Model) => {
+          <EditModel addModel={(model: Model) => {
             setModels(models.push(model));
           }} />
         </tbody>
