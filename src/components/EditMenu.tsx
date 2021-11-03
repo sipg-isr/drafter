64e40acc0bf3349342c8fdb8e2f9971d97799506
Andrew Lee <andrew.lambda@tuta.io>
2021-11-03 12:36:20 +0000

    Add edit menu UI (#4)
    * feat(ui): create EditMenu
    
    * misc(EditMenu): add dummy functionality to the edit menu

---
+++ src/components/EditMenu.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {
+  Container,
+  ListGroup
+} from 'react-bootstrap';
+
+export default function EditMenu() {
+  const dummy = () => {
+    // TODO actually do something here
+    console.log('Not yet implemented');
+  };
+  return (
+    <Container>
+      <ListGroup horizontal>
+        <ListGroup.Item action onClick={dummy}>Open</ListGroup.Item>
+        <ListGroup.Item action onClick={dummy}>Save</ListGroup.Item>
+        <ListGroup.Item action onClick={dummy}>Export</ListGroup.Item>
+      </ListGroup>
+    </Container>
+  );
+}
+
