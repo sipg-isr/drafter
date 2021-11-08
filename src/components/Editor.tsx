53d256bdec25b7479eeeb6cc58d4f6ce35daea58
Andrew Lee <andrewtl@stanford.edu>
2021-11-08 13:53:59 +0000

    feat: delete unused edges

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect } from 'react';
 import {
   Col,
   Row
@@ -13,13 +13,31 @@ import {
 } from '../types';
 import Sidebar from './Sidebar';
 import Graph from './Graph';
-import {
-  instantiateModel
-} from '../utils';
+import { lookupAccessPoint } from '../utils';
+import { useEdges, useModels, useNodes } from '../state';
 
 export default function Editor() {
   // TODO store state somewhere like localStorage or idb-keyval
   // write a custom hook to serialize / deserialize this
+  const [models, ] = useModels();
+  const [nodes, setNodes] = useNodes();
+  const [edges, setEdges] = useEdges();
+
+  useEffect(() => {
+    // Whenever the models are changed,
+    // filter out only the nodes that are from these models
+    const newNodes = nodes.filter(node =>
+      models.find(({ modelId }) => node.modelId === modelId));
+    setNodes(newNodes);
+  }, [models]);
+
+  useEffect(() => {
+    // Whenever the nodes are changed, keep only the edges with surviving points
+    setEdges(edges.filter(({ requesterId, responderId }) =>
+      lookupAccessPoint(nodes, requesterId) !== null &&
+      lookupAccessPoint(nodes, responderId) !== null));
+  }, [nodes]);
+
   return (
     <>
       <Row>
--- src/components/Models.tsx
+++ src/components/Models.tsx
@@ -20,26 +20,16 @@ export default function Models() {
   // To add an entry by pushing it into the entries set
   const addEntry = (entry: ModelEntry) => setEntries(entries.push(entry));
 
-  const [models , setModels] = useModels();
-  const [nodes, setNodes] = useNodes();
-  const [edges, setEdges] = useEdges();
+  const [, setModels] = useModels();
 
   useEffect(() => {
-    // Models have been updated
+    // Whenever the model entries are updated, update the models
     setModels(Set(
       entries
         .filter(({ model }) => model !== null)
         .map(({ model }) => model!)
     ));
   }, [entries]);
-
-  useEffect(() => {
-    // Filter out only the nodes that are from these models
-    const newNodes = nodes.filter(node =>
-      models.find(({ modelId }) => node.modelId === modelId));
-
-    setNodes(newNodes);
-  }, [models]);
 
   return (
     <>
