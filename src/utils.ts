12ed44d145deebb34a3385b692f585d03f5cd504
Andrew Lee <andrewtl@stanford.edu>
2021-11-09 12:02:33 +0000

    fix: put requester and responder in the right order in export function

--- src/utils.ts
+++ src/utils.ts
@@ -229,10 +229,10 @@ export async function exportState({ models, nodes, edges }: State): Promise<Blob
     })).toArray(),
     links: edges.map(({ requesterId, responderId }) => ({
       source: {
-        stage: nodes.find(({ nodeId }) => nodeId === requesterId.nodeId)?.name || 'Node not found'
+        stage: nodes.find(({ nodeId }) => nodeId === responderId.nodeId)?.name || 'Node not found'
       },
       target: {
-        stage: nodes.find(({ nodeId }) => nodeId === responderId.nodeId)?.name || 'Node not found'
+        stage: nodes.find(({ nodeId }) => nodeId === requesterId.nodeId)?.name || 'Node not found'
       }
     })).toArray()
   };
