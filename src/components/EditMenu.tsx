4780b9d8404438197226d363ad6157ef7bfcbcc6
Andrew Lee <andrewtl@stanford.edu>
2021-11-10 13:11:30 +0000

    feat(ui): add clear modal

--- src/components/EditMenu.tsx
+++ src/components/EditMenu.tsx
@@ -16,7 +16,7 @@ import { saveAs } from 'file-saver';
  * The different possible dialogs that can be displayed to the user
  */
 enum DialogOption {
-  Save, Load, Export
+  Save, Load, Export, Clear
 }
 
 interface SaveDialogProps {
@@ -123,12 +123,33 @@ function ExportDialog({ show, close }: ExportDialogProps) {
   );
 }
 
+interface ClearDialogProps {
+  show: boolean;
+  close: () => void;
+}
+function ClearDialog({ show, close }: ClearDialogProps) {
+  return (
+    <Modal show={show} onEscapeKeyDown={close}>
+      <Modal.Header>
+        <Modal.Title>Really clear the editor?</Modal.Title>
+        <CloseButton onClick={close} />
+      </Modal.Header>
+      <Modal.Body>
+        <Button variant="primary" onClick={close}>Cancel</Button>
+        &nbsp;
+        <Button variant="danger" onClick={() => {}}>Clear</Button>
+      </Modal.Body>
+    </Modal>
+  );
+}
+
 export default function EditMenu() {
   const [currentDialog, setCurrentDialog] = useState<DialogOption | null>(null);
 
   // Control whether the save dialog is open
   const openSaveDialog = () => setCurrentDialog(DialogOption.Save);
   const openLoadDialog = () => setCurrentDialog(DialogOption.Load);
+  const openClearDialog = () => setCurrentDialog(DialogOption.Clear);
   const openExportDialog = () => setCurrentDialog(DialogOption.Export);
   const closeDialog = () => setCurrentDialog(null);
 
@@ -137,11 +158,13 @@ export default function EditMenu() {
       <ListGroup horizontal>
         <ListGroup.Item action onClick={openSaveDialog}>Save</ListGroup.Item>
         <ListGroup.Item action onClick={openLoadDialog}>Load</ListGroup.Item>
+        <ListGroup.Item action onClick={openClearDialog}>Clear</ListGroup.Item>
         <ListGroup.Item action onClick={openExportDialog}>Export</ListGroup.Item>
       </ListGroup>
 
       <SaveDialog show={currentDialog === DialogOption.Save} close={closeDialog} />
       <LoadDialog show={currentDialog === DialogOption.Load} close={closeDialog} />
+      <ClearDialog show={currentDialog === DialogOption.Clear} close={closeDialog} />
       <ExportDialog show={currentDialog === DialogOption.Export} close={closeDialog} />
     </Container>
   );
