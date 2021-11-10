059d25d9ea5c0d2822589bb012d9ee64825cedfc
Andrew Lee <andrewtl@stanford.edu>
2021-11-10 15:11:00 +0000

    feat: move state into a reducer

--- src/components/EditMenu.tsx
+++ src/components/EditMenu.tsx
@@ -8,7 +8,7 @@ import {
   ListGroup,
   Modal
 } from 'react-bootstrap';
-import { State, useStore } from '../state';
+import { State, useStore, useDispatch } from '../state';
 import { deserializeState, exportState, fileContent, serializeState } from  '../utils';
 import { saveAs } from 'file-saver';
 
@@ -59,8 +59,9 @@ interface LoadDialogProps {
 }
 function LoadDialog({ show, close }: LoadDialogProps) {
 
+  const dispatch = useDispatch();
+
   const fileUploadRef = useRef<HTMLInputElement | null>(null);
-  const restoreState = useStore(state => state.restoreState);
 
   return (
     <Modal show={show} onEscapeKeyDown={close}>
@@ -75,7 +76,7 @@ function LoadDialog({ show, close }: LoadDialogProps) {
         <Button onClick={async () => {
           const content = await fileContent(fileUploadRef!.current!);
           if (content) {
-            restoreState(deserializeState(content));
+            dispatch({ kind: 'RestoreState', state: deserializeState(content) })
           }
           close();
         }}>Load</Button>
@@ -131,7 +132,7 @@ interface ClearDialogProps {
   close: () => void;
 }
 function ClearDialog({ show, close }: ClearDialogProps) {
-  const clearState = useStore(store => store.clearState);
+  const dispatch = useDispatch();
   return (
     <Modal show={show} onEscapeKeyDown={close}>
       <Modal.Header>
@@ -142,7 +143,7 @@ function ClearDialog({ show, close }: ClearDialogProps) {
         <Button variant='primary' onClick={close}>Cancel</Button>
         &nbsp;
         <Button variant='danger' onClick={() => {
-          clearState();
+          dispatch({ kind: 'ClearState' });
           close();
         }}>Clear</Button>
       </Modal.Body>
--- src/state.ts
+++ src/state.ts
@@ -10,6 +10,47 @@ import {
   UUID
 } from './types';
 
+interface SetModels {
+  kind: 'SetModels';
+  models: Set<Model>;
+}
+
+interface SetNodes {
+  kind: 'SetNodes';
+  nodes: Set<Node>;
+}
+
+interface SetEdges {
+  kind: 'SetEdges';
+  edges: Set<Edge>;
+}
+
+interface RestoreState {
+  kind: 'RestoreState';
+  state: State;
+}
+
+interface ClearState {
+  kind: 'ClearState';
+}
+
+type Action = SetModels | SetNodes | SetEdges | RestoreState | ClearState;
+
+function reducer(state: State, action: Action): State {
+  switch (action.kind) {
+    case 'SetModels':
+      return { ...state, models: action.models };
+    case 'SetNodes':
+      return { ...state, nodes: action.nodes };
+    case 'SetEdges':
+      return { ...state, edges: action.edges };
+    case 'RestoreState':
+      return action.state;
+    case 'ClearState':
+      return { ...state, models: Set(), nodes: Set(), edges: Set() };
+  }
+}
+
 /**
  * The global state for the application. This requires a few properties
  *
@@ -17,40 +58,33 @@ import {
 export interface State {
   /** A set of models that have been onboarded onto the platform  */
   models: Set<Model>;
-  setModels: (models: Set<Model>) => void;
 
   /** A set of nodes that were instantiated into the graph */
   nodes: Set<Node>;
-  setNodes: (nodes: Set<Node>) => void;
 
   /** A set of edges that connect the nodes in the graph */
   edges: Set<Edge>;
-  setEdges: (edges: Set<Edge>) => void;
 
-  /** Set the entire state, from new */
-  restoreState: (state: State) => void;
-
-  /** Reset the state to default values */
-  clearState: () => void;
+  dispatch: (action: Action) => void;
 }
 
 export const useStore = create<State>(set => ({
   models:    Set(),
-  setModels: models => set(() => ({ models })),
   nodes:    Set(),
-  setNodes: nodes => set(() => ({ nodes })),
   edges:    Set(),
-  setEdges: edges => set(() => ({ edges })),
-  restoreState: state => set(state, false),
-  clearState: () => set({ models: Set(), nodes: Set(), edges: Set() }, false)
+  dispatch: action => set(state => reducer(state, action))
 }));
 
+export function useDispatch() {
+  return useStore(state => state.dispatch);
+}
+
 export function useModels(): [Set<Model>, (models: Set<Model>) => void] {
-  return useStore(state => [state.models, state.setModels]);
+  return useStore(state => [state.models, ((models: Set<Model>) => state.dispatch({ kind: 'SetModels', models }))]);
 }
 export function useNodes(): [Set<Node>, (nodes: Set<Node>) => void] {
-  return useStore(state => [state.nodes, state.setNodes]);
+  return useStore(state => [state.nodes, ((nodes: Set<Node>) => state.dispatch({ kind: 'SetNodes', nodes }))]);
 }
 export function useEdges(): [Set<Edge>, (edges: Set<Edge>) => void] {
-  return useStore(state => [state.edges, state.setEdges]);
+  return useStore(state => [state.edges, ((edges: Set<Edge>) => state.dispatch({ kind: 'SetEdges', edges }))]);
 }
