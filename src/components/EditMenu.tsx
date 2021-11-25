9595eedd13f3e444f8c98904475330c8eae610ff
Andrew Lee <andrewtl@stanford.edu>
2021-11-25 16:48:00 +0000

    rebase onto develop

--- src/components/EditMenu.tsx
+++ src/components/EditMenu.tsx
@@ -2,18 +2,43 @@ import React, { useState } from 'react';
 import {
   Container,
   ListGroup,
-  Modal
+  Modal,
+  CloseButton
 } from 'react-bootstrap';
 import SaveDialog from './modals/SaveDialog';
 import LoadDialog from './modals/LoadDialog';
 import ClearDialog from './modals/ClearDialog';
 import ExportDialog from './modals/ExportDialog';
+import { useStore } from '../state';
 
 /**
  * The different possible dialogs that can be displayed to the user
  */
 enum DialogOption {
-  Save, Load, Export, Clear
+  Save, Load, Export, Clear, Debug
+}
+
+
+interface DebugDialogProps {
+  show: boolean;
+  close: () => void;
+}
+function DebugDialog({ show, close }: DebugDialogProps) {
+  const state = useStore();
+  return (
+    <Modal show={show} onEscapeKeyDown={close}>
+      <Modal.Header>
+        <Modal.Title>Debug Info</Modal.Title>
+        <CloseButton onClick={close} />
+      </Modal.Header>
+      <Modal.Body>
+        <h5>State</h5>
+        <pre>
+          {JSON.stringify(state, null, 2)}
+        </pre>
+      </Modal.Body>
+    </Modal>
+  );
 }
 
 export default function EditMenu() {
@@ -24,6 +49,7 @@ export default function EditMenu() {
   const openLoadDialog = () => setCurrentDialog(DialogOption.Load);
   const openClearDialog = () => setCurrentDialog(DialogOption.Clear);
   const openExportDialog = () => setCurrentDialog(DialogOption.Export);
+  const openDebugDialog = () => setCurrentDialog(DialogOption.Debug);
   const closeDialog = () => setCurrentDialog(null);
 
   return (
@@ -33,12 +59,14 @@ export default function EditMenu() {
         <ListGroup.Item action onClick={openLoadDialog}>Load</ListGroup.Item>
         <ListGroup.Item action onClick={openClearDialog}>Clear</ListGroup.Item>
         <ListGroup.Item action onClick={openExportDialog}>Export</ListGroup.Item>
+        <ListGroup.Item action style={{ whiteSpace: 'nowrap' }}onClick={openDebugDialog}>Debug Info</ListGroup.Item>
       </ListGroup>
 
       <SaveDialog show={currentDialog === DialogOption.Save} close={closeDialog} />
       <LoadDialog show={currentDialog === DialogOption.Load} close={closeDialog} />
       <ClearDialog show={currentDialog === DialogOption.Clear} close={closeDialog} />
       <ExportDialog show={currentDialog === DialogOption.Export} close={closeDialog} />
+      <DebugDialog show={currentDialog === DialogOption.Debug} close={closeDialog} />
     </Container>
   );
 }
