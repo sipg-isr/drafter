46b6412f9dcaf53b8cda11549c4f9412f71a62f5
Andrew Lee <andrewtl@stanford.edu>
2021-11-29 15:05:43 +0000

    feat: add copying feature to listing

--- src/components/Listing.tsx
+++ src/components/Listing.tsx
@@ -1,4 +1,9 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
+import { FaCopy } from 'react-icons/fa';
+
+// This is the the basic unit of size
+const unit = 5;
 
 interface ListingProps {
   content: string
@@ -7,10 +12,26 @@ export default function Listing({ content }: ListingProps) {
   // TODO make this div style configurable based on the current theme?
   // TODO add syntax highlighting?
   return <pre style={{
-    borderRadius: '5px',
+    position: 'relative',
+    borderRadius: `${unit}px`,
     backgroundColor: '#eee',
-    padding: '10px',
+    padding: `${3 * unit}px`,
   }}>
     <code>{content}</code>
+    &nbsp;
+    <Button
+      variant='outline-secondary'
+      size='sm'
+      style={{
+        position: 'absolute',
+          right: `${unit}px`,
+          top: `${2 * unit}px`
+      }}
+      onClick={() =>
+        navigator.clipboard.writeText(content)
+      }
+    >
+      <FaCopy size={3 * unit}/>
+    </Button>
   </pre>
 }
