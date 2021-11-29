ab2494573f8c77edefbb8926e4ecb706fa2b12fc
Andrew Lee <andrewtl@stanford.edu>
2021-11-29 16:27:46 +0000

    feat: show applicaiton state in debug info

--- src/components/modals/DebugDialog.tsx
+++ src/components/modals/DebugDialog.tsx
@@ -25,25 +25,37 @@ export function DebugDialog({ show, close }: DebugDialogProps) {
         <h5>Edit History</h5>
         <Accordion>
           {state.actions.reverse().map(([date, action], idx) =>
-            <Accordion.Item eventKey={idx.toString()} key={idx}>
-              <Accordion.Header>
-                <span style={{
-                  textAlign: 'left',
+          <Accordion.Item eventKey={idx.toString()} key={idx}>
+            <Accordion.Header>
+              <span style={{
+                textAlign: 'left',
                   width: '40%',
                   display: 'inline-block'
-                }}>{action.type}</span>
-                <em style={{
-                  textAlign: 'right',
+              }}>{action.type}</span>
+              <em style={{
+                textAlign: 'right',
                   width: '50%',
                   display: 'inline-block'
-                }}
-                >{date.toLocaleString()}
-                </em>
-              </Accordion.Header>
-              <Accordion.Body>
-                <Listing content={JSON.stringify(action, null, 2)} />
-              </Accordion.Body>
-            </Accordion.Item>)}
+              }}
+              >{date.toLocaleString()}
+              </em>
+            </Accordion.Header>
+            <Accordion.Body>
+              <Listing content={JSON.stringify(action, null, 2)} />
+            </Accordion.Body>
+          </Accordion.Item>)}
+        </Accordion>
+        <br />
+        <h5>Application State</h5>
+        <Accordion>
+          <Accordion.Item eventKey='state'>
+            <Accordion.Header>
+              State
+            </Accordion.Header>
+            <Accordion.Body>
+              <Listing content={JSON.stringify(state, null, 2)}/>
+            </Accordion.Body>
+          </Accordion.Item>
         </Accordion>
       </Modal.Body>
     </Modal>
