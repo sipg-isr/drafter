567a52c4976dc561bb350441fb0847b0141190b9
Andrew Lee <andrew.lambda@tuta.io>
2021-12-19 23:37:40 -0500

    feat: add output types

---
+++ src/types/output.ts
@@ -0,0 +1,41 @@
+/**
+ * A type to describe docker-compose.yml files
+ */
+export interface DockerCompose {
+  version: '3'
+  services: [
+    string :
+    {
+      image: string
+      volumes: {
+        type: string
+        source: string
+        target: string
+      }[]
+      environment?: [string : string]
+      ports: string[]
+    }
+  ]
+}
+
+/**
+ * A type to describe config.yml files
+ */
+export interface OutputConfig {
+  stages: {
+    name: string
+    host: string
+    port: number
+  }[];
+
+  links: {
+    source: {
+      stage: string;
+      field: string;
+    }
+    target: {
+      stage: string;
+      field: string;
+    }
+  }[]
+}
